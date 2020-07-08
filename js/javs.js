1d0608324a2f9b53bf402c798af940def388bff8
Kostya <zmeishe132451@ya.ru>
2020-07-08 16:48:22 +0500

    Work 12, objects

--- js/javs.js
+++ js/javs.js
@@ -1,2 +1,20 @@
-let numberOfFilms = prompt('Сколько фильмов Вы уже посмотрели?','');
-console.log(numberOfFilms);
+"use strict";
+const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?','');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const lastMovie = prompt('Какой последний фильмы Вы смотрели?'),
+      assessmentMovie = +prompt('Как вы его оцениваете по десятибальной шкале?'),
+      nextLastMovie = prompt('Назовите еще один фильм'),
+      nextAssessmentMovie = +prompt('Как оцените его?');
+
+personalMovieDB.movies[lastMovie]=assessmentMovie;
+personalMovieDB.movies[nextLastMovie]=nextAssessmentMovie;
+
+console.log(personalMovieDB);
