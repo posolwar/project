8d83549802e8e1d92588fc20dc772d75e308aaef
Kostya <zmeishe132451@ya.ru>
2020-07-08 19:14:37 +0500

    #15 cycle

--- js/javs.js
+++ js/javs.js
@@ -1,20 +1,23 @@
 "use strict";
 
-let num = 50;
+let personalMovieDB = {
+    count: 0,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-// while (num <= 55){
-//     console.log(num);
-//     num++;
-// }
+for(let i = 0; i < 2; i++){
+    const a = prompt('Вспомните какой-либо из просмотренных фильмов и напишите название фильма'),
+          b = +prompt('Поставьте ему оценку от 0 до 10');
 
-// do{
-//     console.log(num);
-//     num++;
-// }
-// while (num <= 55);
-
-for(let i = 1; i < 8; i++){
-    if(i === 6) break; // заканчивает for
-    else if(i === 3) continue; //Пропускает цикл
-    console.log(i);
+    if(a != null && b != null && a != '' && b != '' && a.length < 50){
+        personalMovieDB.movies[a] = b;
+        alert('Данные введены верно');
+    } else {
+        alert('Данные ведены неверно! Название фильма должно содержать менее 50ти символов и более 0');
+        i--;
+    }
 }
+console.log(personalMovieDB);
